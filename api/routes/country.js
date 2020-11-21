95f4e44f61d21c686dea5fc6b2aafc2c84a3c517
unknown <seymurpashayev2018@gmail.com>
2020-11-21 23:23:56 +0400

    first commit

---
+++ api/routes/country.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const moongose = require('mongoose');
+const Country = require('../models/country');
+
+router.get('/', (req, res, next) => {
+    Country.find()
+    .then(countries => res.json(countries))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.post('/', (req,res, next) => {
+    const country = new Country({
+    _id: new moongose.Types.ObjectId(),
+    name:req.body.name,
+    capital:req.body.capital
+    });
+    country
+    .save()
+    .then(result => {
+    console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(200).json({
+        message:'Handling POST request to /country',
+        country:country
+    });
+});
+
+router.get('/:countryId',(req, res, next) => {
+const id = req.params.productId;
+if(id === 'special')
+ {
+res.status(200).json({
+message: 'You discovered the special ID',
+id:id
+});
+ }
+else {
+    res.status(200).json({
+        message:'You passed an ID'
+    })
+}
+});
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(cors());
+
+const countryRoutes = require('./api/routes/country');
+
+
+const uri = process.env.uri;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+app.use((res, req, next) => {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 
+'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+if (req.method === "OPTIONS") {
+    res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+}
+next();
+})
+
+app.use('/countries', countryRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status= 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+res.status(error.status || 500);
+res.json({
+    error:{
+        message:error.message
+    }
+});
+});
+module.exports = app;
